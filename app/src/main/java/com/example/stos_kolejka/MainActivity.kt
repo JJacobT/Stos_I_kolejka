a5d2f270e3f90d5d0353121f96be82a90c2e3311
JJacobT <tyszko0526@wp.pl>
2022-11-03 20:57:00 +0100

    zmiana wygladu i dodane zmienne

--- app/src/main/java/com/example/stos_kolejka/MainActivity.kt
+++ app/src/main/java/com/example/stos_kolejka/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.stos_kolejka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.LinkedList
+import java.util.Queue
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        val kolejka: Queue<Int> = LinkedList<Int>()
+        val butt_dodaj_do_kolejki = findViewById<Button>(R.id.but_dodaj)
+        val butt_usun_z_kolejki = findViewById<Button>(R.id.but_usun)
+        val butt_pierw_i_ost_w_kolejce = findViewById<Button>(R.id.but_ostatniel)
+        val txt_ile_w_kolejce = findViewById<TextView>(R.id.ilosc)
+        val txt_pierwszy_w_kolejce = findViewById<TextView>(R.id.ostatni_element)
+        val txt_ostatni_w_kolejce = findViewById<TextView>(R.id.pierwszy_element)
+        val length = Toast.LENGTH_SHORT
+        var numer = 1
+        var ilosc_kolejka = 0
+
+
+            }
+        }
+
